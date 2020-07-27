5ec82fb842768d1a3855317efd3ce1fa22dcba86
Nik <61439310+NikV2@users.noreply.github.com>
2020-07-27 10:38:37 +0300

    Commit

--- src/main/java/me/nik/combatplus/handlers/UpdateChecker.java
+++ src/main/java/me/nik/combatplus/handlers/UpdateChecker.java
@@ -1,17 +1,19 @@
 package me.nik.combatplus.handlers;
 
 import me.nik.combatplus.CombatPlus;
-import me.nik.combatplus.listeners.UpdateReminder;
 import me.nik.combatplus.managers.MsgType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
-public class UpdateChecker extends BukkitRunnable {
+public class UpdateChecker extends BukkitRunnable implements Listener {
 
     private final CombatPlus plugin;
 
@@ -24,18 +26,32 @@ public class UpdateChecker extends BukkitRunnable {
     @Override
     public void run() {
         try {
-            HttpsURLConnection connection = (HttpsURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=76788").openConnection();
-            String version = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
-
-            if (!plugin.getDescription().getVersion().equalsIgnoreCase(version)) {
-                newVersion = version;
-                plugin.consoleMessage(MsgType.UPDATE_REMINDER.getMessage().replaceAll("%current%", plugin.getDescription().getVersion()).replaceAll("%new%", newVersion));
-                plugin.registerEvent(new UpdateReminder(plugin, this));
-            } else {
-                plugin.consoleMessage(MsgType.CONSOLE_UPDATE_NOT_FOUND.getMessage());
-            }
-        } catch (IOException ignored) {
+            newVersion = readLines();
+        } catch (IOException e) {
+            plugin.getLogger().warning("Couldn't check for updates, Is the server connected to the internet?");
         }
+
+        if (!plugin.getDescription().getVersion().equals(newVersion)) {
+            plugin.consoleMessage(MsgType.UPDATE_REMINDER.getMessage().replaceAll("%current%", plugin.getDescription().getVersion()).replaceAll("%new%", newVersion));
+            plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        } else {
+            plugin.consoleMessage(MsgType.CONSOLE_UPDATE_NOT_FOUND.getMessage());
+        }
+    }
+
+    private String readLines() throws IOException {
+        URLConnection connection = new URL("https://raw.githubusercontent.com/NikV2/CombatPlus/master/version.txt").openConnection();
+        connection.addRequestProperty("User-Agent", "Mozilla/4.0");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        return reader.readLine();
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        if (!e.getPlayer().hasPermission("cp.admin")) return;
+        e.getPlayer().sendMessage(MsgType.UPDATE_REMINDER.getMessage().replaceAll("%current%", plugin.getDescription().getVersion()).replaceAll("%new%", newVersion));
     }
 
     public String getNewVersion() {
